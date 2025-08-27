eab732bc833a0d89754a7aa8a0b56e930e838649
Julierme Peixoto <julierme2005@hotmail.com>
2025-08-26 21:22:57 -0300

    first commit

---
+++ dags/fourth_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+with DAG('fourth_dag',
+          description='First DAG', 
+          schedule_interval=None,
+          start_date=datetime(2025, 1, 1),
+          catchup=False) as dag:
+
+    task1 = BashOperator(task_id='tsk1', bash_command='sleep 5')
+    task2 = BashOperator(task_id='tsk2', bash_command='sleep 5')
+    task3 = BashOperator(task_id='tsk3', bash_command='sleep 5')
+
+    task1.set_downstream(task2)
+    task2.set_downstream(task3)
---
+++ dags/trigger_dag3.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+dag = DAG('trigger_dag3',
+          description='First DAG', 
+          schedule_interval=None,
+          start_date=datetime(2025, 1, 1),
+          catchup=False)
+
+task1 = BashOperator(task_id='tsk1', bash_command='exit 1', dag=dag)
+task2 = BashOperator(task_id='tsk2', bash_command='exit 1', dag=dag)
+task3 = BashOperator(task_id='tsk3', bash_command='sleep 5', trigger_rule='all_failed', dag=dag)
+
+[task1, task2] >> task3
